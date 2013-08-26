a7e29b87c12c637f02dba95e21a33199891ef871
Kuznetsov Mikhail <kuznetsov@adwz.ru>
2013-08-26 12:25:03 +0400

    updated interface;

--- Source/UIViewController+EXModalPickerExtension.h
+++ Source/UIViewController+EXModalPickerExtension.h
@@ -14,6 +14,12 @@
 @property (nonatomic, strong) EXModalPickerController *exPickerController;
 
 - (void)showModalPicker;
+
+- (void)showModalPicker:(EXModalPickerController *)controller;
+
+- (void)showModalPicker:(EXModalPickerController *)controller inView:(UIView *)rootView;
+
 - (void)hideModalPicker;
 
+- (void)hideModalPicker:(EXModalPickerController *)controller;
 @end
